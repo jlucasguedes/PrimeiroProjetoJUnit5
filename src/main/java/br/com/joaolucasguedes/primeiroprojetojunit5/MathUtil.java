3b1a3ae775d046e8cb5a24f6adc1d942a019b9f6
João Lucas Guedes de Oliveira <iftolucas@gmail.com>
2021-05-23 19:42:50 -0300

    Primeiro Projeto JUnit5 #2.3 👩🏽‍🔬 Aplicando TDD ☀️ 19/05 🌒 10/05
    Realize a implementação do último vídeo de TDD com JUnit. Além das propriedades do MDC mostradas no vídeo, você deve implementar as propriedades 2 a 7.

--- src/main/java/br/com/joaolucasguedes/primeiroprojetojunit5/MathUtil.java
+++ src/main/java/br/com/joaolucasguedes/primeiroprojetojunit5/MathUtil.java
@@ -12,9 +12,30 @@ package br.com.joaolucasguedes.primeiroprojetojunit5;
 public class MathUtil {
 
     public static int mdc(int a, int b) {
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        //Proprieddade 6
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+
+        //Proprieddade 1
         if (b > 0 && a % b == 0) {
             return b;
         }
+
+        //Proprieddade 3
+        if (b == 0) {
+            return Math.abs(a);
+        }
+
+        //Proprieddade 5
+        if (a % b != 0) {
+            return 1;
+        }
+
         return -1;
     }
 
